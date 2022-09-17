fa532b107e1450121f01ecc26f544ea6617178af
lichchang <lichChang1997@gmail.com>
2022-09-17 18:46:11 -0400

    Update Jobs.java

--- src/main/java/com/group21/jobTracker/backend/data/Jobs.java
+++ src/main/java/com/group21/jobTracker/backend/data/Jobs.java
@@ -1,20 +1,29 @@
 package com.group21.jobTracker.backend.data;
 
 import java.io.Serializable;
-import java.util.Objects;
-import java.util.Set;
+import java.util.Date;
+//import javax.validation.constraints.*;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 public class Jobs implements Serializable{
 	
-	@NotNull
-    private int id = -1;
-    @Size(min = 2, message = "Category name must be at least two characters")
-    private String name;
-    private Set<Category> category;
+	//@NotNull(message="id must not be null")
+    private int id;
+	
+    //@NotNull(message="jobTitle must not be null")
+    private String jobTitle;
     
+    //@NotNull(message="company must not be null")
+    private String company;
+    
+    //@NotNull(message="dateApplied must not be null")
+	private Date dateApplied;
+    
+	private String nextAction;
+	private String status;
+	private int priority;
+	 
+    /*Is id necessary? */
     public int getId() {
         return id;
     }
@@ -22,50 +31,52 @@ public class Jobs implements Serializable{
     public void setId(int id) {
         this.id = id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return getName();
-    }
-
-    /*
-     * Vaadin DataProviders rely on properly implemented equals and hashcode
-     * methods.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null || id == -1) {
-            return false;
-        }
-        if (obj instanceof Jobs) {
-            return id == ((Jobs) obj).id;
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        if (id == -1) {
-            return super.hashCode();
-        }
-
-        return Objects.hash(id);
-    }
-
-	 public Set<Category> getCategory() {
-        return category;
-    }
-
-    public void setCategory(Set<Category> category) {
-        this.category = category;
-    }
-
+    
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	public Date getDateApplied() {
+		return dateApplied;
+	}
+	public void setDateApplied(Date dateApplied) {
+		this.dateApplied = dateApplied;
+	}
+	
+	public String getNextAction() {
+		return nextAction;
+	}
+	public void setNextAction(String nextAction) {
+		this.nextAction = nextAction;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStaus(String status) {
+		if (!status.equals("In progress") && !status.equals("Rejected") && !status.equals("Accepted")) {
+			throw new IllegalArgumentException();
+		}
+		this.status = status;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		if (priority > 3 || priority < 1) {
+			throw new IllegalArgumentException();
+		}
+		this.priority = priority;
+	}
 }
