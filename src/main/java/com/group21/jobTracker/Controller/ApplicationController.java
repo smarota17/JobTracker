36299a5450af1be81050108c77a83d76d81e3bee
saminur <saminur.islam08@gmail.com>
2022-09-20 21:12:08 -0400

    send the written functions in Application Controller for review

--- src/main/java/com/group21/jobTracker/Controller/ApplicationController.java
+++ src/main/java/com/group21/jobTracker/Controller/ApplicationController.java
@@ -1,5 +1,59 @@
 package com.group21.jobTracker.Controller;
 
-public class ApplicationController {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.group21.jobTracker.backend.DataService;
+import com.group21.jobTracker.backend.data.Jobs;
+
+@Controller
+public class ApplicationController{
+	
+	@Autowired
+	DataService dbService;
+	
+	@GetMapping("/")
+	public String WelcomePage() {
+		return "index";
+	}
+	
+private String jobTitle;
+    
+    //@NotNull(message="company must not be null")
+    private String company;
+    
+    //@NotNull(message="dateApplied must not be null")
+	private Date dateApplied;
+    
+	private String nextAction;
+	private String status;
+	private int priority;
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addJob(HttpServletRequest request, HttpServletResponse response) throws ParseException {
+		
+		String jobTitle = request.getParameter("jobTitle");
+		String company = request.getParameter("company");
+		String dateApplied = request.getParameter("date");
+		String status = request.getParameter("status");
+		
+		Jobs job = new Jobs();
+		job.setJobTitle(jobTitle);
+		job.setCompany(company);
+		job.setDateApplied(new SimpleDateFormat("dd/MM/yyyy").parse(dateApplied));
+		job.setStaus(status);
+		
+		dbService.updateJob(job);
+		return "add";
+	}
+	
 }
