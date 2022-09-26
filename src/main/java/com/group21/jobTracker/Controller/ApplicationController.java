33e73a60bee1b31a14b80dd5ff24052eb7398876
saminur <saminur.islam08@gmail.com>
2022-09-26 00:12:10 -0400

    add update and delete api in the controller

--- src/main/java/com/group21/jobTracker/Controller/ApplicationController.java
+++ src/main/java/com/group21/jobTracker/Controller/ApplicationController.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,19 +30,8 @@ public class ApplicationController{
 		return "index";
 	}
 	
-private String jobTitle;
-    
-    //@NotNull(message="company must not be null")
-    private String company;
-    
-    //@NotNull(message="dateApplied must not be null")
-	private Date dateApplied;
-    
-	private String nextAction;
-	private String status;
-	private int priority;
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String addJob(HttpServletRequest request, HttpServletResponse response) throws ParseException {
+	public ResponseEntity<Jobs> addJob(HttpServletRequest request, HttpServletResponse response) throws ParseException {
 		
 		String jobTitle = request.getParameter("jobTitle");
 		String company = request.getParameter("company");
@@ -53,7 +45,32 @@ private String jobTitle;
 		job.setStaus(status);
 		
 		dbService.updateJob(job);
-		return "add";
+		//HttpHeaders httpHeaders = new HttpHeaders();
+		//httpHeaders.add("new job", "/job/" + String.valueOf(job.getId()));
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/updateJob", method = RequestMethod.POST)
+	public ResponseEntity<Jobs> updateJobs(HttpServletRequest request, HttpServletResponse response) throws ParseException {
+		
+		
+		String id = request.getParameter("id");
+		String next_action = request.getParameter("next action");
+		String status = request.getParameter("status");
+		Jobs jobToUpdate = dbService.getJobsbyId(Integer.parseInt(id));
+		
+		jobToUpdate.setNextAction(next_action);
+		jobToUpdate.setStaus(status);
+		dbService.updateJob(jobToUpdate);
+		return new ResponseEntity<>(dbService.getJobsbyId(Integer.parseInt(id)), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public ResponseEntity<Jobs> deleteJobs(HttpServletRequest request, HttpServletResponse response) throws ParseException {
+			
+		String id = request.getParameter("id");
+		dbService.deleteJob(Integer.parseInt(id));
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 }
