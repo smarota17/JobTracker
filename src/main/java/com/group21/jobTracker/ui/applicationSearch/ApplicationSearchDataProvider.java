94978665cdece81eb42b9bbfb4fcb25fe7d9c5c2
saminur <saminur.islam08@gmail.com>
2022-10-06 16:05:13 -0400

    adjusting apicalls for null user in mock service

--- src/main/java/com/group21/jobTracker/ui/applicationSearch/ApplicationSearchDataProvider.java
+++ src/main/java/com/group21/jobTracker/ui/applicationSearch/ApplicationSearchDataProvider.java
@@ -40,7 +40,7 @@ public class ApplicationSearchDataProvider extends ListDataProvider<Jobs> {
         User currentUser;
         
 		try {
-			currentUser = Csv.loadUser(MainLayout.userName);
+			currentUser = Csv.loadUser(MainLayout.userName.replaceAll("\\s", ""));
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("Cannot read/write to file.");
 		} catch (ParseException e) {
